45b5393af563cbf3b2731c019ce9e20099e44eab
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-08 18:49:27 +0200

    Add entrypoint script

---
+++ src/scripts/app/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+cd $SCRIPTS_FOLDER
+
+# Source an init script that a child image may have added
+if [ -x ./startup.sh ]; then
+	source ./startup.sh
+fi
+
+echo 'Starting app...'
+cd $APP_BUNDLE_FOLDER/bundle
+
+exec "$@"
