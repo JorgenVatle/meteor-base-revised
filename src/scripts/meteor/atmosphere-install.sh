b16eb767b22e630a5b28853540bc965ef13f13b8
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-08 21:14:10 +0200

    Update wording for meteor package installer

--- src/scripts/meteor/atmosphere-install.sh
+++ src/scripts/meteor/atmosphere-install.sh
@@ -2,11 +2,12 @@
 
 set -o errexit
 
-printf "\n[-] Installing Meteor's Atmosphere dependencies...\n\n"
+printf "\n[-] Installing Meteor packages...\n\n"
 
 cd $APP_SOURCE_FOLDER
 
 # Remove package.json files to prevent Meteor from trying to access mainModule files that might not exist yet.
 rm package*.json
 
-meteor lint
+# Running the Meteor lint command seems to be the most reliable way to force Meteor to install local dependencies.
+meteor lint || echo "WARN: Failed to run Meteor linter - this only impacts caching for the next build step, so not a huge concern" && exit 0
