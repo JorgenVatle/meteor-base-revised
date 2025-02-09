b681aa832b8917eeb183122999d0ac4d53ffb7a2
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-09 04:16:33 +0200

    Ignore failure on package.json remove error

--- src/scripts/meteor/atmosphere-install.sh
+++ src/scripts/meteor/atmosphere-install.sh
@@ -7,7 +7,7 @@ printf "\n[-] Installing Meteor packages...\n\n"
 cd $APP_SOURCE_FOLDER
 
 # Remove package.json files to prevent Meteor from trying to access mainModule files that might not exist yet.
-rm package*.json
+rm package*.json || exit 0
 
 # Running the Meteor lint command seems to be the most reliable way to force Meteor to install local dependencies.
 meteor lint || echo "WARN: Failed to run Meteor linter - this only impacts caching for the next build step, so not a huge concern" && exit 0
