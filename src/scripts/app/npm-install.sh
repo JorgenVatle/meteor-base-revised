130c865a53a6b671b39a8ec4cc2bcff4d3901160
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-11 13:37:44 +0300

    Add missing double-quotes for file paths

--- src/scripts/app/npm-install.sh
+++ src/scripts/app/npm-install.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 printf "\n[-] Installing final application's npm dependencies...\n\n"
 
-cd $APP_BUNDLE_FOLDER/bundle/programs/server/
+cd "$APP_BUNDLE_FOLDER/bundle/programs/server/"
 npm install
 
 if [[ "$1" = '--build-from-source' ]]; then
 	npm rebuild --build-from-source
-	cd $APP_BUNDLE_FOLDER/bundle/programs/server/npm
+	cd "$APP_BUNDLE_FOLDER/bundle/programs/server/npm"
 	npm rebuild --build-from-source
 fi
